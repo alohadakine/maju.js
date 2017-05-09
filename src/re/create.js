e3a7237c5b1fec21bc103e45097f3cafe0181f75
Marco Kellershoff <marco.kellershoff@gofeminin.de>
2017-05-09 11:43:28 +0200

    Make create.js awesome

--- src/re/create.js
+++ src/re/create.js
@@ -1,6 +1,24 @@
-window.re.prototype.create = function(name) {
-	var el;
+window.re.prototype.create = function(name, contents, attrs) {
+	var el, contentsCallback, attrsCallback;
 	el = document.createElement(name);
+	contentsCallback = function(content) {
+		el.appendChild(content);
+	};
+	attrsCallback = function(attr) {
+		el.setAttribute(attr[0], attr[1]);
+	};
+	if (contents) {
+		if (typeof contents === 'string') {
+			el.innerHTML = contents;
+		} else if (typeof contents === 'object' && contents.length) {
+			contents.forEach(contentsCallback);
+		} else if (typeof contents === 'object' && !contents.length) {
+			el.appendChild(contents);
+		}
+	}
+	if (attrs) {
+		attrs.forEach(attrsCallback);
+	}
 	return window.re.find(el).get(0);
 };
 
