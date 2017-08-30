6d97f87dcee8477a4b0d54862f1a314864b49e9b
Marco Kellershoff <marco.kellershoff@gmail.com>
2017-08-30 12:45:07 +0200

    Fix `getFileContents`

--- src/re/getFileContents.js
+++ src/re/getFileContents.js
@@ -2,14 +2,14 @@ window.re.prototype.getFileContents = function(file, cb) {
   if (file) {
     var reader = new FileReader();
     reader.readAsText(file, "UTF-8");
-    reader.onload = function (){
-      cb(false, fileRef);
+    reader.onload = function(evt) {
+      cb(false, evt.target.result, fileRef);
     };
-    reader.onerror = function (){
-      cb("Error reading file", fileRef);
+    reader.onerror = function() {
+      cb("Error reading file", null, fileRef);
     };
   } else {
-    cb("fileRef not found");
+    cb("file ref not found", null, null);
   }
 };
 
