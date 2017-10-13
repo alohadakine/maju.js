02b6ea2513474b051b013c31886776705e7a2e84
Marco Kellershoff <marco.kellershoff@gmail.com>
2017-10-13 13:14:07 +0200

    Change EditorConfig and add compareJson

--- dist/re.js
+++ dist/re.js
@@ -110,6 +110,28 @@ window.re.prototype.before = function(el, beforeEl) {
 	return el;
 };
 
+window.re.prototype.compareJson = function(el) {
+        var aIsInB = function(a, b) {
+                for (var prop in a) {
+                        if (a.hasOwnProperty(prop)) {
+                                if (b.hasOwnProperty(prop)) {
+                                        if (typeof a[prop] === "object") {
+                                                if (!aIsInB(a[prop], b[prop]))
+                                                        return false;
+                                        } else {
+                                                if (a[prop] !== b[prop])
+                                                        return false;
+                                        }
+                                } else {
+                                        return false;
+                                }
+                        }
+                }
+                return true;
+        };
+        return (aIsInB(a, b) && aIsInB(b, a));
+};
+
 window.re.prototype.create = function(name, contents, attrs, is) {
 	var el, contentsCallback, attrsCallback;
   if (is)
---
+++ src/re/compareJson.js
@@ -0,0 +1,22 @@
+window.re.prototype.compareJson = function(el) {
+        var aIsInB = function(a, b) {
+                for (var prop in a) {
+                        if (a.hasOwnProperty(prop)) {
+                                if (b.hasOwnProperty(prop)) {
+                                        if (typeof a[prop] === "object") {
+                                                if (!aIsInB(a[prop], b[prop]))
+                                                        return false;
+                                        } else {
+                                                if (a[prop] !== b[prop])
+                                                        return false;
+                                        }
+                                } else {
+                                        return false;
+                                }
+                        }
+                }
+                return true;
+        };
+        return (aIsInB(a, b) && aIsInB(b, a));
+};
+
