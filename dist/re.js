0062df3a02fe6e64dbede11d42c26b99f64cd40a
Marco Kellershoff <marco.kellershoff@gmail.com>
2017-10-13 14:10:33 +0200

    Update compiled JS version

--- dist/re.js
+++ dist/re.js
@@ -1,7 +1,7 @@
-window.re = function() {};
-window.re.prototype.adBlockEnabled = function(cb) {
-        var js = document.createElement('script');
-        js.src = 'https://js.revsci.net/gateway/gw.js';
+var re = function() {};
+re.prototype.adBlockEnabled = function(cb) {
+        var js = document.createElement("script");
+        js.src = "https://js.revsci.net/gateway/gw.js";
         js.onerror = function() {
                 // AdBlock is enabled
                 cb(true);
@@ -17,19 +17,19 @@ window.re.prototype.adBlockEnabled = function(cb) {
         document.body.appendChild(js);
 };
 
-window.re.prototype.after = function(el, afterEl) {
+re.prototype.after = function(el, afterEl) {
 	el.parentNode.insertBefore(afterEl, el.nextElementSibling);
 	return el;
 };
 
-window.re.prototype.ajax = function(opts) {
+re.prototype.ajax = function(opts) {
 	var formatParams, getTimestamp, header, params, xhr;
 	opts = opts || {};
-	opts.method = opts.method || 'GET';
+	opts.method = opts.method || "GET";
 	opts.method = opts.method.toUpperCase();
 	opts.cb = opts.cb || function() {};
 	opts.params = opts.params || {};
-	opts.requestContentType = opts.requestContentType || 'application/x-www-form-urlencoded';
+	opts.requestContentType = opts.requestContentType || "application/x-www-form-urlencoded";
 	getTimestamp = function() {
 		return Math.floor(Date.now() / 1000);
 	};
@@ -37,37 +37,35 @@ window.re.prototype.ajax = function(opts) {
 		var arr, key;
 		arr = [];
 		for (key in params) {
-			arr.push(encodeURIComponent(key) + '=' + encodeURIComponent(params[key]));
+			arr.push(encodeURIComponent(key) + "=" + encodeURIComponent(params[key]));
 		}
 		return arr;
 	};
 	params = formatParams(opts.params);
 	xhr = new XMLHttpRequest();
-	if (opts.method === 'GET' && params.length) {
-		opts.url = opts.url + '?' + params.join('&');
+	if (opts.method === "GET" && params.length) {
+		opts.url = opts.url + "?" + params.join("&");
 	}
-	if (opts.method === 'GET' && opts.cache === false) {
-		if (opts.url.indexOf('?') === -1) {
-			opts.url = opts.url + '?_nocache=' + getTimestamp();
-		} else {
-			opts.url = opts.url + '&_nocache=' + getTimestamp();
-		}
+	if (opts.method === "GET" && opts.cache === false) {
+		if (opts.url.indexOf("?") === -1)
+			opts.url = opts.url + "?_nocache=" + getTimestamp();
+		else
+			opts.url = opts.url + "&_nocache=" + getTimestamp();
 	}
 	xhr.open(opts.method, opts.url);
-	xhr.setRequestHeader('Content-type', opts.requestContentType);
+	xhr.setRequestHeader("Content-type", opts.requestContentType);
 	if (opts.headers) {
 		for (header in opts.headers) {
 			xhr.setRequestHeader(header.key, header.value);
 		}
 	}
-	if (opts.method === 'GET') {
+	if (opts.method === "GET") {
 		xhr.send(null);
 	} else {
-		if (params.length) {
-			xhr.send(params.join('&'));
-		} else {
+		if (params.length)
+			xhr.send(params.join("&"));
+		else
 			xhr.send(null);
-		}
 	}
 	xhr.onreadystatechange = function() {
 		var DONE, OK, err;
@@ -75,26 +73,25 @@ window.re.prototype.ajax = function(opts) {
 		OK = 200;
 		err = false;
 		if (xhr.readyState === DONE) {
-			if (xhr.status !== OK) {
+			if (xhr.status !== OK)
 				err = xhr.status;
-			}
 			opts.cb(err, xhr.responseText);
 		}
 	};
 };
 
-window.re.prototype.append = function(el, appendEl) {
-  if (appendEl.length) {
-    appendEl.forEach(function(tmp){
-      el.appendChild(tmp);
-    });
-  } else {
-    el.appendChild(appendEl);
-  }
-	return el;
+re.prototype.append = function(el, appendEl) {
+        if (appendEl.length) {
+                appendEl.forEach(function(tmp) {
+                        el.appendChild(tmp);
+                });
+        } else {
+                el.appendChild(appendEl);
+        }
+        return el;
 };
 
-window.re.prototype.attr = function(el, key, value) {
+re.prototype.attr = function(el, key, value) {
 	var returnvalue;
 	returnvalue = el;
 	if (value) {
@@ -105,12 +102,12 @@ window.re.prototype.attr = function(el, key, value) {
 	return returnvalue;
 };
 
-window.re.prototype.before = function(el, beforeEl) {
+re.prototype.before = function(el, beforeEl) {
 	el.parentNode.insertBefore(beforeEl, el);
 	return el;
 };
 
-window.re.prototype.compareJson = function(el) {
+re.prototype.compareJson = function(el) {
         var aIsInB = function(a, b) {
                 for (var prop in a) {
                         if (a.hasOwnProperty(prop)) {
@@ -132,7 +129,7 @@ window.re.prototype.compareJson = function(el) {
         return (aIsInB(a, b) && aIsInB(b, a));
 };
 
-window.re.prototype.create = function(name, contents, attrs, is) {
+re.prototype.create = function(name, contents, attrs, is) {
 	var el, contentsCallback, attrsCallback;
   if (is)
     el = document.createElement(name, is);
@@ -145,31 +142,31 @@ window.re.prototype.create = function(name, contents, attrs, is) {
 		el.setAttribute(attr[0], attr[1]);
 	};
 	if (contents) {
-		if (typeof contents === 'string') {
+		if (typeof contents === "string") {
 			el.innerHTML = contents;
-		} else if (typeof contents === 'object' && contents.length) {
+		} else if (typeof contents === "object" && contents.length) {
 			contents.forEach(contentsCallback);
-		} else if (typeof contents === 'object' && !contents.length) {
+		} else if (typeof contents === "object" && !contents.length) {
 			el.appendChild(contents);
 		}
 	}
 	if (attrs) {
 		attrs.forEach(attrsCallback);
 	}
-	return window.re.find(el).get(0);
+	return re.find(el).get(0);
 };
 
-window.re.prototype.domready = function(cb) {
-	if (document.readyState === 'complete') {
+re.prototype.domready = function(cb) {
+	if (document.readyState === "complete") {
 		return window.setTimeout(cb, 1);
 	} else {
-		document.addEventListener('DOMContentLoaded', function() {
+		document.addEventListener("DOMContentLoaded", function() {
 			cb();
 		});
 	}
 };
 
-window.re.prototype.each = function(arr, cb) {
+re.prototype.each = function(arr, cb) {
 	var child, i, j, len;
 	for (i = j = 0, len = arr.length; j < len; i = ++j) {
 		child = arr[i];
@@ -178,11 +175,11 @@ window.re.prototype.each = function(arr, cb) {
 	return arr;
 };
 
-window.re.prototype.find = function(sel, ref) {
+re.prototype.find = function(sel, ref) {
 	var el, el_arr, els, i, len;
 	el_arr = [];
 	if (sel) {
-		if (typeof sel !== 'object') {
+		if (typeof sel !== "object") {
 			if (ref) {
 				els = ref.querySelectorAll(sel);
 			} else {
@@ -198,45 +195,45 @@ window.re.prototype.find = function(sel, ref) {
 		for (i = 0, len = els.length; i < len; i++) {
 			el = els[i];
 			el.find = function(sel) {
-				return window.re.find(sel, this);
+				return re.find(sel, this);
 			};
 			el.on = function(eventName, cb) {
-				return window.re.on(this, eventName, cb);
+				return re.on(this, eventName, cb);
 			};
 			el.attr = function(key, value) {
-				return window.re.attr(this, key, value);
+				return re.attr(this, key, value);
 			};
 			el.html = function(value) {
-				return window.re.html(this, value);
+				return re.html(this, value);
 			};
 			el.prepend = function(prependEl) {
-				return window.re.prepend(this, prependEl);
+				return re.prepend(this, prependEl);
 			};
 			el.append = function(appendEl) {
-				return window.re.append(this, appendEl);
+				return re.append(this, appendEl);
 			};
 			el.after = function(afterEl) {
-				return window.re.after(this, afterEl);
+				return re.after(this, afterEl);
 			};
 			el.before = function(beforeEl) {
-				return window.re.before(this, beforeEl);
+				return re.before(this, beforeEl);
 			};
 			el.parent = function() {
-				return window.re.parent(this);
+				return re.parent(this);
 			};
 			el.next = function() {
-				return window.re.next(this);
+				return re.next(this);
 			};
 			el.prev = function() {
-				return window.re.prev(this);
+				return re.prev(this);
 			};
 			el.nodes = function() {
-				return window.re.nodes(this);
+				return re.nodes(this);
 			};
 			el_arr.push(el);
 		}
 		el_arr.each = function(cb) {
-			return window.re.each(this, cb);
+			return re.each(this, cb);
 		};
 		el_arr.get = function(index) {
 			return this[index];
@@ -245,22 +242,22 @@ window.re.prototype.find = function(sel, ref) {
 	return el_arr;
 };
 
-window.re.prototype.getFileContents = function(file, cb) {
-  if (file) {
-    var reader = new FileReader();
-    reader.readAsText(file, "UTF-8");
-    reader.onload = function(evt) {
-      cb(false, evt.target.result, file);
-    };
-    reader.onerror = function() {
-      cb("Error reading file", null, file);
-    };
-  } else {
-    cb("file ref not found", null, null);
-  }
+re.prototype.getFileContents = function(file, cb) {
+        if (file) {
+                var reader = new FileReader();
+                reader.readAsText(file, "UTF-8");
+                reader.onload = function(evt) {
+                        cb(false, evt.target.result, file);
+                };
+                reader.onerror = function() {
+                        cb("Error reading file", null, file);
+                };
+        } else {
+                cb("file ref not found", null, null);
+        }
 };
 
-window.re.prototype.html = function(el, value) {
+re.prototype.html = function(el, value) {
 	var returnvalue;
 	returnvalue = el;
 	if (typeof value === "string") {
@@ -271,23 +268,24 @@ window.re.prototype.html = function(el, value) {
 	return returnvalue;
 };
 
-window.re.prototype.loadjs = function(urls, cb) {
-	var appendscript, i, len, url, y;
-	y = window.re;
-	appendscript = function(url, cb) {
-		var s;
-		s = y.create('script').attr('type', 'text/javascript').attr('async', 'true').attr('src', url);
+re.prototype.loadjs = function(urls, cb) {
+	var i, len, url;
+	var appendscript = function(url, cb) {
+		var s = re.create("script")
+                        .attr("type", "text/javascript")
+                        .attr("async", "true")
+                        .attr("src", url);
 		if (cb) {
-			s.on('error', function() {
+			s.on("error", function() {
 				cb(true, this);
 			});
-			s.on('load', function() {
+			s.on("load", function() {
 				cb(false, this);
 			});
 		}
-		return y.find('script').get(0).prepend(s);
+		return re.find("script").get(0).prepend(s);
 	};
-	if (typeof urls === 'string') {
+	if (typeof urls === "string") {
 		urls = [urls];
 	}
 	for (i = 0, len = urls.length; i < len; i++) {
@@ -296,38 +294,38 @@ window.re.prototype.loadjs = function(urls, cb) {
 	}
 };
 
-window.re.prototype.next = function(el) {
-	return window.re.find(el.nextElementSibling).get(0);
+re.prototype.next = function(el) {
+	return re.find(el.nextElementSibling).get(0);
 };
 
-window.re.prototype.nodes = function(el) {
+re.prototype.nodes = function(el) {
 	var child, child_arr, i, len, ref;
 	child_arr = [];
 	ref = el.children;
 	for (i = 0, len = ref.length; i < len; i++) {
 		child = ref[i];
-		child_arr.push(window.re.find(child).get(0));
+		child_arr.push(re.find(child).get(0));
 	}
 	return child_arr;
 };
 
-window.re.prototype.on = function(el, eventName, cb) {
+re.prototype.on = function(el, eventName, cb) {
 	el.addEventListener(eventName, cb, false);
 	return el;
 };
 
-window.re.prototype.parent = function(el) {
-	return window.re.find(el.parentNode).get(0);
+re.prototype.parent = function(el) {
+	return re.find(el.parentNode).get(0);
 };
 
-window.re.prototype.plugins = {};
-window.re.prototype.prepend = function(el, prependEl) {
+re.prototype.plugins = {};
+re.prototype.prepend = function(el, prependEl) {
 	el.parentNode.insertBefore(prependEl, el);
 	return el;
 };
 
-window.re.prototype.prev = function(el) {
-	return window.re.find(el.previousElementSibling).get(0);
+re.prototype.prev = function(el) {
+	return re.find(el.previousElementSibling).get(0);
 };
 
 window.re = new window.re();
