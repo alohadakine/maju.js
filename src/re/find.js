941f9f17cbd895296f6e1b4fcd595c4da1a47781
Marco Kellershoff <marco.kellershoff@gmail.com>
2017-10-13 14:58:08 +0200

    Create functions outside of the loop

--- src/re/find.js
+++ src/re/find.js
@@ -1,6 +1,45 @@
 re.prototype.find = function(sel, ref) {
-	var el, el_arr, els, i, len;
+	var el_arr, els, i, len;
 	el_arr = [];
+        var forEachElementCallback = function(el) {
+                el.find = function(sel) {
+                        return re.find(sel, this);
+                };
+                el.on = function(eventName, cb) {
+                        return re.on(this, eventName, cb);
+                };
+                el.attr = function(key, value) {
+                        return re.attr(this, key, value);
+                };
+                el.html = function(value) {
+                        return re.html(this, value);
+                };
+                el.prepend = function(prependEl) {
+                        return re.prepend(this, prependEl);
+                };
+                el.append = function(appendEl) {
+                        return re.append(this, appendEl);
+                };
+                el.after = function(afterEl) {
+                        return re.after(this, afterEl);
+                };
+                el.before = function(beforeEl) {
+                        return re.before(this, beforeEl);
+                };
+                el.parent = function() {
+                        return re.parent(this);
+                };
+                el.next = function() {
+                        return re.next(this);
+                };
+                el.prev = function() {
+                        return re.prev(this);
+                };
+                el.nodes = function() {
+                        return re.nodes(this);
+                };
+                el_arr.push(el);
+        };
 	if (sel) {
 		if (typeof sel !== "object") {
 			if (ref) {
@@ -16,44 +55,7 @@ re.prototype.find = function(sel, ref) {
 			}
 		}
 		for (i = 0, len = els.length; i < len; i++) {
-			el = els[i];
-			el.find = function(sel) {
-				return re.find(sel, this);
-			};
-			el.on = function(eventName, cb) {
-				return re.on(this, eventName, cb);
-			};
-			el.attr = function(key, value) {
-				return re.attr(this, key, value);
-			};
-			el.html = function(value) {
-				return re.html(this, value);
-			};
-			el.prepend = function(prependEl) {
-				return re.prepend(this, prependEl);
-			};
-			el.append = function(appendEl) {
-				return re.append(this, appendEl);
-			};
-			el.after = function(afterEl) {
-				return re.after(this, afterEl);
-			};
-			el.before = function(beforeEl) {
-				return re.before(this, beforeEl);
-			};
-			el.parent = function() {
-				return re.parent(this);
-			};
-			el.next = function() {
-				return re.next(this);
-			};
-			el.prev = function() {
-				return re.prev(this);
-			};
-			el.nodes = function() {
-				return re.nodes(this);
-			};
-			el_arr.push(el);
+			forEachElementCallback(els[i]);
 		}
 		el_arr.each = function(cb) {
 			return re.each(this, cb);
